e671719c6229b81ce0331c844c3c99079ff976d0
jaquevan <jaquevan@bu.edu>
2024-12-10 21:28:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -451,6 +451,7 @@ def get_wishlist() -> Response:
         return make_response(jsonify({'error': str(e)}), 500)
 
 if __name__ == "__main__":
+
     app.run(debug=True, host="0.0.0.0", port=5000)
 
 
