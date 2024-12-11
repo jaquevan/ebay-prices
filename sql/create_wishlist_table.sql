4d8cba0f5ff871385eac0d150c6c80f939bf857e
Zhi <zhilinlu12@gmail.com>
2024-12-10 19:01:01 -0500

    editted wishlist table attributes and renamed table to items

--- sql/create_wishlist_table.sql
+++ sql/create_wishlist_table.sql
@@ -1,10 +1,11 @@
-DROP TABLE IF EXISTS wishlist;
-CREATE TABLE wishlist (
+DROP TABLE IF EXISTS items;
+CREATE TABLE items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    item_id TEXT NOT NULL,
+    ebay_item_id TEXT NOT NULL,
     title TEXT NOT NULL,
     price REAL,
+    available_quantity = INTEGER NOT NULL,
+    sold_quantity = INTEGER NOT NULL,
     alert_price REAL,
     deleted BOOLEAN DEFAULT FALSE
 );
